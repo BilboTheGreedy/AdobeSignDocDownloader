565dc4d2feb2b456ac902133c8a8bdc667f88d2e
BilboTheGreedy <daniel.rapp@live.se>
2023-03-02 11:42:26 +0100

    some prompts and stuff

--- main.go
+++ main.go
@@ -9,8 +9,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"reflect"
+	"regexp"
 	"sync"
 	"time"
+
+	"github.com/manifoldco/promptui"
 )
 
 type Agreements struct {
@@ -221,44 +225,105 @@ func DownloadAgreement(ACCESSTOKEN string, baseUri string, AgreementID string, U
 	return nil
 }
 
-func main() {
+var r, _ = regexp.Compile("\\\\|/|:|\\*|\\?|<|>")
+
+// MakeFilenameWindowsFriendly removes characters not permitted in file/directory names on Windows
+func MakeFilenameWindowsFriendly(name string) string {
+	return r.ReplaceAllString(name, "")
+}
+
+func Run(ACCESSTOKEN string, out string, groupName string, groupID string, selectAUser bool, pVerbose bool) {
+
 	start := time.Now()
-	accessTokenPtr := flag.String("accesstoken", "none", "API Access Token")
-	outPathPtr := flag.String("out", "C:\\documents", "Download Location")
-	pVerbose := flag.Bool("v", false, "Explain what's happening while program runs")
-	flag.Parse()
-	fmt.Println("Access Token:", *accessTokenPtr)
-	fmt.Println("out path:", *outPathPtr)
-	fmt.Fprintf(os.Stdout, "Verbose mode set? %v\n", *pVerbose)
+
+	var count = 0
 	var wg sync.WaitGroup
-	var ACCESSTOKEN = *accessTokenPtr
 	baseuri := baseURI{}
-	groups := Response{}
+	var gn []string
+	var UserID string
+	var UserEmail string
+	var agreementsCount int
 	groupMembers := GroupMembers{}
 	agreements := Agreements{}
-	file_path := *outPathPtr
+	file_path := out
 
-	GetURI(ACCESSTOKEN, &baseuri, *pVerbose)
+	GetURI(ACCESSTOKEN, &baseuri, pVerbose)
 
-	GetGroups(ACCESSTOKEN, baseuri.APIAccessPoint, &groups, *pVerbose)
-	for _, group := range groups.GroupInfoList {
-		fmt.Println("GroupName:", group.GroupName+"\tGroupID:", group.GroupID)
-		GetGroupsMembers(ACCESSTOKEN, baseuri.APIAccessPoint, &groupMembers, group.GroupID, *pVerbose)
-		fmt.Println("Main : Submitting job to workers")
-		for _, user := range groupMembers.UserInfoList {
-			fmt.Println("Email:", user.Email+"\tID:", user.ID)
-			GetUserAgreements(ACCESSTOKEN, baseuri.APIAccessPoint, &agreements, user.ID, *pVerbose)
-			agreementsCount := len(agreements.UserAgreementList)
-			wg.Add(agreementsCount)
-			for _, agreemment := range agreements.UserAgreementList {
-				fmt.Println("Downloading document:", agreemment.Name+" ID:", agreemment.ID)
-				fullPath := file_path + "\\" + group.GroupName + "\\" + user.Email + "\\" + agreemment.Name + "(" + agreemment.ID + ")\\"
-				go DownloadAgreement(ACCESSTOKEN, baseuri.APIAccessPoint, agreemment.ID, user.ID, fullPath, agreemment.Name+".pdf", &wg, *pVerbose)
-			}
+	//GetGroups(ACCESSTOKEN, baseuri.APIAccessPoint, &groups, pVerbose)
+
+	fmt.Println("GroupName:", groupName+"\tGroupID:", groupID)
+	GetGroupsMembers(ACCESSTOKEN, baseuri.APIAccessPoint, &groupMembers, groupID, pVerbose)
+	for _, v := range groupMembers.UserInfoList {
+		gn = append(gn, v.Email)
+	}
+	prompt := promptui.Select{
+		Label: "Select User",
+		Items: gn,
+		Size:  20,
+	}
+
+	_, selectedUser, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("You choose %q\n", selectedUser)
+	for _, v := range groupMembers.UserInfoList {
+		if v.Email == selectedUser {
+			UserID = v.ID
+			UserEmail = v.Email
 		}
 	}
-	fmt.Println("Waiting for goroutines to finish...")
+	fmt.Println("Main : Submitting job to workers")
+
+	//Selected User
+	if selectAUser {
+		fmt.Println("Email:", UserEmail+"\tID:", UserID)
+		GetUserAgreements(ACCESSTOKEN, baseuri.APIAccessPoint, &agreements, UserID, pVerbose)
+
+		for _, v := range agreements.UserAgreementList {
+			if v.Status == "SIGNED" {
+				agreementsCount++
+			}
+
+		}
+		wg.Add(agreementsCount)
+		count += agreementsCount
+
+		for _, agreemment := range agreements.UserAgreementList {
+			if agreemment.Status == "SIGNED" {
+				fmt.Println("\t- Downloading document:", agreemment.Name)
+				fullPath := file_path + "\\" + groupName + "\\" + UserEmail + "\\" + MakeFilenameWindowsFriendly(agreemment.Name) + "(" + agreemment.ID + ")\\"
+				go DownloadAgreement(ACCESSTOKEN, baseuri.APIAccessPoint, agreemment.ID, UserID, fullPath, MakeFilenameWindowsFriendly(agreemment.Name)+".pdf", &wg, pVerbose)
+			}
+
+		}
+		fmt.Println("Count:", count)
+	} else {
+
+		//All Users
+		for _, user := range groupMembers.UserInfoList {
+			fmt.Println("Email:", user.Email+"\tID:", user.ID)
+			GetUserAgreements(ACCESSTOKEN, baseuri.APIAccessPoint, &agreements, user.ID, pVerbose)
+			agreementsCount := len(agreements.UserAgreementList)
+			wg.Add(agreementsCount)
+			count += agreementsCount
+
+			for _, agreemment := range agreements.UserAgreementList {
+				fmt.Println("\t- Downloading document:", agreemment.Name)
+				fullPath := file_path + "\\" + groupName + "\\" + user.Email + "\\" + MakeFilenameWindowsFriendly(agreemment.Name) + "(" + agreemment.ID + ")\\"
+				go DownloadAgreement(ACCESSTOKEN, baseuri.APIAccessPoint, agreemment.ID, user.ID, fullPath, MakeFilenameWindowsFriendly(agreemment.Name)+".pdf", &wg, pVerbose)
+			}
+			fmt.Println("Count:", count)
+		}
+
+	}
+
+	fmt.Println("Main: Waiting for goroutines to finish before next group...")
 	wg.Wait()
+
 	fmt.Println("Done!")
 	// Code to measure
 	duration := time.Since(start)
@@ -268,4 +333,86 @@ func main() {
 
 	// Nanoseconds as int64
 	fmt.Println(duration.Nanoseconds())
+}
+func GetStructFieldName(Struct interface{}, StructField ...interface{}) (fields map[int]string) {
+	fields = make(map[int]string)
+
+	for r := range StructField {
+		s := reflect.ValueOf(Struct).Elem()
+		f := reflect.ValueOf(StructField[r]).Elem()
+
+		for i := 0; i < s.NumField(); i++ {
+			valueField := s.Field(i)
+			if valueField.Addr().Interface() == f.Addr().Interface() {
+				fields[i] = s.Type().Field(i).Name
+			}
+		}
+	}
+	return fields
+}
+
+func main() {
+	accessTokenPtr := flag.String("accesstoken", "none", "API Access Token")
+	outPathPtr := flag.String("out", "C:\\documents", "Download Location")
+	pVerbose := flag.Bool("v", false, "Explain what's happening while program runs")
+
+	flag.Parse()
+	fmt.Println("Access Token:", *accessTokenPtr)
+	fmt.Println("out path:", *outPathPtr)
+	fmt.Fprintf(os.Stdout, "Verbose mode set? %v\n", *pVerbose)
+
+	var ACCESSTOKEN = *accessTokenPtr
+	baseuri := baseURI{}
+	groups := Response{}
+	var selectUser bool
+	var gn []string
+	var groupid string
+	var groupname string
+	GetURI(ACCESSTOKEN, &baseuri, *pVerbose)
+
+	GetGroups(ACCESSTOKEN, baseuri.APIAccessPoint, &groups, *pVerbose)
+
+	for _, v := range groups.GroupInfoList {
+		gn = append(gn, v.GroupName)
+	}
+
+	prompt := promptui.Select{
+		Label: "Select Group",
+		Items: gn,
+		Size:  20,
+	}
+	promptB := promptui.Prompt{
+		Label:     "Select Specifc User?",
+		IsConfirm: true,
+	}
+
+	selectAUser, err := promptB.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("You choose %q\n", selectAUser)
+	if selectAUser == "y" {
+		selectUser = true
+	} else {
+		selectUser = false
+	}
+
+	_, selectedGroup, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	for _, v := range groups.GroupInfoList {
+		if v.GroupName == selectedGroup {
+			groupid = v.GroupID
+			groupname = v.GroupName
+		}
+	}
+	fmt.Printf("You choose %q\n", selectedGroup)
+	Run(ACCESSTOKEN, *outPathPtr, groupname, groupid, selectUser, *pVerbose)
 }
